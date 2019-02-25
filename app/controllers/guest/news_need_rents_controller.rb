6e6186c87f8404c21152dce95b172a178f65fe83
iamPhong <truongvanphong97@gmail.com>
2019-02-25 10:07:44 +0700

    Edit news need rent

--- app/controllers/guest/news_need_rents_controller.rb
+++ app/controllers/guest/news_need_rents_controller.rb
@@ -1,6 +1,8 @@
 class Guest::NewsNeedRentsController < ApplicationController
   before_action :authenticate_user!
-  before_action :list_category, only: :new
+  before_action :set_rent, except: %i(new create)
+  before_action :list_category, only: %i(edit new)
+  before_action :correct_user, only: %i(edit update)
 
   def new
     @rent = NewsNeedRent.new
@@ -17,10 +19,35 @@ class Guest::NewsNeedRentsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @rent.update rent_params
+      flash[:success] = t "complete"
+      redirect_to guest_news_need_rent_path
+    else
+      flash[:danger] = t "error"
+    end
+  end
+
+  def show; end
+
   private
 
   def rent_params
     params.require(:news_need_rent).permit(:user_id, :category_id, :product_name, :description, :picture,
      :address, :phone_number)
+  end
+
+  def set_rent
+    @rent = NewsNeedRent.find_by id: params[:id]
+    return if @rent
+    flash[:danger] = t "notfound"
+    redirect_to new_guest_news_need_rent_path
+  end
+
+  def correct_user
+    @rent = current_user.news_need_rents.find_by id: params[:id]
+    redirect_to root_url unless @rent
   end
 end
