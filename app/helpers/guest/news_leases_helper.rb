daa9b24b3122686b2edcc5aa208485fc39dbf042
iamPhong <truongvanphong97@gmail.com>
2019-02-25 10:07:44 +0700

    Update News Lease

--- app/helpers/guest/news_leases_helper.rb
+++ app/helpers/guest/news_leases_helper.rb
@@ -1,2 +1,11 @@
 module Guest::NewsLeasesHelper
+  def format_price(price_type)
+    if price_type == 0
+      t "day"
+    elsif price_type == 1
+      t ".week"
+    else
+      t ".month"
+    end
+  end
 end
